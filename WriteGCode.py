1e59939ff7e78272ad226041ef78e6e76729a27e
Ghostkeeper <rubend@tutanota.com>
2019-02-05 23:22:06 +0100

    Implement basic writing of g-code
    This should do for now for the g-code part. Now for the layer visualisation part so that I can iterate faster...

--- WriteGCode.py
+++ WriteGCode.py
@@ -4,8 +4,14 @@
 #This plug-in is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for details.
 #You should have received a copy of the GNU Affero General Public License along with this plug-in. If not, see <https://gnu.org/licenses/>.
 
-import cura.LayerDataBuilder
+import cura.Settings.ExtruderManager #To get settings from the active extruder.
+import cura.LayerDataBuilder #One of the results of the function.
+import math
 import typing
+import UM.Logger
+
+from . import ExtrudeCommand #To differentiate between the command types.
+from . import TravelCommand #To differentiate between the command types.
 
 def write_gcode(commands) -> typing.Tuple[str, cura.LayerDataBuilder.LayerDataBuilder]:
 	"""
@@ -14,4 +20,61 @@ def write_gcode(commands) -> typing.Tuple[str, cura.LayerDataBuilder.LayerDataBu
 	:return: A g-code string that would print the commands, as well as a layer
 	data builder that represents the same file for layer view.
 	"""
-	return ("", cura.LayerDataBuilder.LayerDataBuilder()) #TODO: Implement.
+	#Cache some settings we'll use often.
+	extruder_number = cura.Settings.ExtruderManager.ExtruderManager.getInstance().activeExtruderIndex
+	extruder_stack = cura.Settings.ExtruderManager.ExtruderManager.getInstance().getActiveExtruderStack()
+	layer_height_0 = extruder_stack.getProperty("layer_height_0", "value")
+	material_flow = extruder_stack.getProperty("material_flow", "value")
+	material_diameter = extruder_stack.getProperty("material_diameter", "value")
+	machine_gcode_flavor = extruder_stack.getProperty("machine_gcode_flavor", "value") #Necessary to track if we need to extrude volumetric or lengthwise.
+	is_volumetric = machine_gcode_flavor in {"UltiGCode", "RepRap (Volumetric)"}
+	speed_travel = extruder_stack.getProperty("speed_travel", "value") * 60 #Convert to mm/min for g-code.
+	speed_print = extruder_stack.getProperty("speed_wall_0", "value") * 60 #Convert to mm/min for g-code.
+
+	builder = cura.LayerDataBuilder.LayerDataBuilder()
+	builder.addLayer(0)
+	layer = builder.getLayer(0)
+	layer.setHeight(0)
+	layer.setThickness(layer_height_0)
+	gcodes = []
+
+	x = 0
+	y = 0
+	e = 0
+	f = 0
+
+	for command in commands:
+		gcode = ";Unknown command of type {typename}!".format(typename=command.__class__.__name__)
+		if isinstance(command, TravelCommand.TravelCommand):
+			gcode = "G0"
+			if command.x != x:
+				x = command.x
+				gcode += " X{x}".format(x=x)
+			if command.y != y:
+				y = command.y
+				gcode += " Y{y}".format(y=y)
+			if speed_travel != f:
+				f = speed_travel
+				gcode += " F{f}".format(f=f)
+		elif isinstance(command, ExtrudeCommand.ExtrudeCommand):
+			distance = math.sqrt((command.x - x) * (command.x - x) + (command.y - y) * (command.y - y))
+			mm3 = distance * layer_height_0 * command.line_width * material_flow
+			delta_e = mm3 if is_volumetric else mm3 * math.pi * material_diameter * material_diameter / 4
+
+			gcode = "G1"
+			if command.x != x:
+				x = command.x
+				gcode += " X{x}".format(x=x)
+			if command.y != y:
+				y = command.y
+				gcode += " Y{y}".format(y=y)
+			if speed_print != f:
+				f = speed_print
+				gcode += " F{f}".format(f=f)
+			if delta_e != 0:
+				e += delta_e
+				gcode += " E{e}".format(e=e)
+		gcodes.append(gcode)
+
+	UM.Logger.Logger.log("d", "\n".join(gcodes))
+	return "\n".join(gcodes), cura.LayerDataBuilder.LayerDataBuilder() #TODO: Implement.
