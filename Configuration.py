f70c702617bfcfb4ffa2e934e60cb8a370006a15
Ghostkeeper <rubend@tutanota.com>
2021-06-18 16:44:51 +0200

    Fix crash when clicking two buttons
    If two close or cancel events are being triggered in quick succession, Cura would crash. It would release the lock twice, which raises an error. Therefore we'll first re-acquire the lock first, but only if necessary (not blocking). This makes sure we always have the lock before releasing it.
    It's not 100% thread safe still, but these events are not processed in parallel so we're okay there.

--- Configuration.py
+++ Configuration.py
@@ -1,5 +1,5 @@
 # Cura plug-in to read SVG files as toolpaths.
-# Copyright (C) 2020 Ghostkeeper
+# Copyright (C) 2021 Ghostkeeper
 # This plug-in is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 # This plug-in is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for details.
 # You should have received a copy of the GNU Affero General Public License along with this plug-in. If not, see <https://gnu.org/licenses/>.
@@ -79,6 +79,7 @@ class Configuration(PyQt5.QtCore.QObject):
 		"""
 		self._status = UM.Mesh.MeshReader.MeshReader.PreReadResult.accepted
 		self.ui_element.close()
+		self._ui_lock.acquire(blocking=False)
 		self._ui_lock.release()
 
 	@PyQt5.QtCore.pyqtSlot()
@@ -88,6 +89,7 @@ class Configuration(PyQt5.QtCore.QObject):
 		"""
 		self._status = UM.Mesh.MeshReader.MeshReader.PreReadResult.cancelled
 		self.ui_element.close()
+		self._ui_lock.acquire(blocking=False)
 		self._ui_lock.release()
 
 	heightChanged = PyQt5.QtCore.pyqtSignal()
