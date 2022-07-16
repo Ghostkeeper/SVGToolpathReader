cc060ad414c4ef5c231d7941f86fe531b99b3434
Ghostkeeper <rubend@tutanota.com>
2022-07-16 13:57:33 +0200

    Attempt at supporting Cura 5
    We'll need to port everything to Qt6, mainly.
    This doesn't quite work. It thinks the functions of Configuration.py are properties, not functions.

--- Configuration.py
+++ Configuration.py
@@ -1,17 +1,22 @@
 # Cura plug-in to read SVG files as toolpaths.
-# Copyright (C) 2021 Ghostkeeper
+# Copyright (C) 2022 Ghostkeeper
 # This plug-in is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 # This plug-in is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for details.
 # You should have received a copy of the GNU Affero General Public License along with this plug-in. If not, see <https://gnu.org/licenses/>.
 
 import os.path  # To find the QML file to display.
-import PyQt5.QtCore  # To display an interface to the user.
+try:
+	from PyQt6.QtCore import QObject, pyqtProperty, pyqtSignal, pyqtSlot, Qt  # To display an interface to the user.
+	qt_version = 6
+except ImportError:
+	from PyQt5.QtCore import QObject, pyqtProperty, pyqtSignal, pyqtSlot, Qt
+	qt_version = 5
 import threading  # To wait for the user to close the UI.
 import UM.Application
 import UM.Mesh.MeshReader  # To return the correct prompt response for the preread.
 import UM.Message  # To show errors.
 
-class Configuration(PyQt5.QtCore.QObject):
+class Configuration(QObject):
 	"""
 	This class shows a dialogue that asks the user how he'd like to load the
 	file.
@@ -19,7 +24,7 @@ class Configuration(PyQt5.QtCore.QObject):
 	You can show the dialogue with the ``prompt`` function.
 	"""
 
-	_show_ui_trigger = PyQt5.QtCore.pyqtSignal()
+	_show_ui_trigger = pyqtSignal()
 	"""
 	Signal to show the UI.
 
@@ -35,7 +40,7 @@ class Configuration(PyQt5.QtCore.QObject):
 		This doesn't create the actual UI yet. That will be created lazily by
 		the ``prompt`` function.
 		"""
-		super().__init__(parent=UM.Application.Application.getInstance())
+		super().__init__(parent=UM.Application.Application.getInstance().getMainWindow())
 		self.ui_element = None
 		self._file_name = None
 		self._ui_lock = threading.Lock()
@@ -72,9 +77,12 @@ class Configuration(PyQt5.QtCore.QObject):
 		self._height = application.getGlobalContainerStack().getProperty("layer_height_0", "value")  # First time showing this dialogue, use one layer as height.
 		qml_path = os.path.join(application.getPluginRegistry().getPluginPath("SVGToolpathReader"), "ConfigurationDialogue.qml")
 		self.ui_element = application.createQmlComponent(qml_path, {"manager": self})
-		self.ui_element.setFlags(self.ui_element.flags() & ~PyQt5.QtCore.Qt.WindowCloseButtonHint & ~PyQt5.QtCore.Qt.WindowMinimizeButtonHint & ~PyQt5.QtCore.Qt.WindowMaximizeButtonHint)
+		if qt_version >= 6:
+			self.ui_element.setFlags(self.ui_element.flags() & Qt.WindowType.WindowCloseButtonHint & Qt.WindowType.WindowMinimizeButtonHint & Qt.WindowType.WindowMaximizeButtonHint)
+		else:
+			self.ui_element.setFlags(self.ui_element.flags() & ~Qt.WindowCloseButtonHint & ~Qt.WindowMinimizeButtonHint & ~Qt.WindowMaximizeButtonHint)
 
-	@PyQt5.QtCore.pyqtSlot()
+	pyqtSlot()
 	def confirm(self):
 		"""
 		Triggered when the user clicks the OK button in the interface.
@@ -87,7 +95,7 @@ class Configuration(PyQt5.QtCore.QObject):
 		self._ui_lock.acquire(blocking=False)
 		self._ui_lock.release()
 
-	@PyQt5.QtCore.pyqtSlot()
+	pyqtSlot()
 	def cancel(self):
 		"""
 		Triggered when the user closes the dialogue rather than clicking OK.
@@ -97,15 +105,15 @@ class Configuration(PyQt5.QtCore.QObject):
 		self._ui_lock.acquire(blocking=False)
 		self._ui_lock.release()
 
-	heightChanged = PyQt5.QtCore.pyqtSignal()
+	heightChanged = pyqtSignal()
 
-	@PyQt5.QtCore.pyqtSlot(int)
+	pyqtSlot(int)
 	def setHeight(self, height):
 		if height != self._height:
 			self._height = height
 			self.heightChanged.emit()
 
-	@PyQt5.QtCore.pyqtProperty(float, notify=heightChanged, fset=setHeight)
+	pyqtProperty(float, notify=heightChanged, fset=setHeight)
 	def height(self):
 		return self._height
 
